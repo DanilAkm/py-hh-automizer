ee3dcbdeb715e95d1ebdf1161002189b0e2c92ec
boyceing <boyceing@boyceing.ru>
2025-03-12 17:59:42 +0000

    auth

--- apply/login.py
+++ apply/login.py
@@ -1,11 +1,40 @@
-# https://hh.ru/oauth/authorize?
-# response_type=code&
-# client_id={client_id}&
-# state={state}&
-# redirect_uri={redirect_uri}
+import requests
+import json
+import os
+from dotenv import load_dotenv # type: ignore
 
-auth_url = 'https://hh.ru/oauth/authorize'
-client_id = ''
-response_type = 'code'
+load_dotenv('../.env')
+client_id = os.getenv('CLIENT_ID')
+client_secret= os.getenv('CLIENT_SECRET')
+
+def get_access_token(cleint_id: str, client_secret: str, grant_type='client_credentials'):
+  params = {
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'grant_type': grant_type 
+  }
+  response = requests.post(url='https://api.hh.ru/token', params=params)
+  return response.json()
+
+def get_user_auth_token(auth_code: str):
+  load_dotenv('../.env')
+  client_id = os.getenv('CLIENT_ID')
+  client_secret= os.getenv('CLIENT_SECRET')
+  
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  params = {
+    'code': auth_code,
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'grant_type': 'authorization_code'
+  }
+
+  response = requests.post(url=' https://api.hh.ru/token', headers=headers ,params=params)
+  return response.json()
 
 
+if __name__ == '__main__':
+    print('idi nahui')
---
+++ backend/server.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+import requests
+import os
+import sys
+from dotenv import load_dotenv # type: ignore
+# import apply.login
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from apply.login import get_user_auth_token
+
+load_dotenv('../.env')
+client_id = os.getenv('CLIENT_ID')
+
+app = FastAPI()
+
+HH_OAUTH_URL = "https://hh.ru/oauth/authorize"
+ 
+@app.get("/login")
+def login():
+    return RedirectResponse(f"{HH_OAUTH_URL}?response_type=code&client_id={client_id}")
+
+@app.get("/oauth")
+def oauth_callback(request: Request, code: str):
+    print(f"Received OAuth code: {code}")
+    auth_token = get_user_auth_token(code)
+    print(auth_token)
+    headers = {
+       "Authorization": f"Bearer {auth_token['access_token']}",
+       'HH-User-Agent': 'boyceing.ru/1.0 boyceing@boyceing.ru'
+    }
+    info = requests.get('https://api.hh.ru/me', headers=headers)
+    return info.json()
